48aee18bf0c2fa5b0dbdac00542b2cff93fdba8b
fathanhadyan <fathanhadyan777@gmail.com>
2022-10-26 22:09:31 +0700

    solve an error

--- project_django/settings.py
+++ project_django/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loveiscaring',
-    'authentication'
+    'authentication',
+    'timeline',
 ]
 
 MIDDLEWARE = [
