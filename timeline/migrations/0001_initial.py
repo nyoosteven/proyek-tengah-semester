d3650fcd82a491cd52337c49e9fe624b66ea6a2c
hapsarimeilany <hapsarimeilany@gmail.com>
2022-11-01 23:12:22 +0700

    ubah halaman profile

--- timeline/migrations/0001_initial.py
+++ timeline/migrations/0001_initial.py
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream:timeline/migrations/0001_initial.py
-=======
-
->>>>>>> Stashed changes:loveiscaring/migrations/0001_initial.py
 
 from django.conf import settings
 from django.db import migrations, models
