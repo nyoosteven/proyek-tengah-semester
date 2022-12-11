1eaf6c25aebf234b4177828624fc4b7f8365c013
fathanhadyan <fathanhadyan777@gmail.com>
2022-12-12 05:34:38 +0700

    add new views for flutter

--- project_django/settings.py
+++ project_django/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'artikel',
     'user_profile',
     'feedback',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_CREDENTIALS=True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 ROOT_URLCONF = 'project_django.urls'
 
